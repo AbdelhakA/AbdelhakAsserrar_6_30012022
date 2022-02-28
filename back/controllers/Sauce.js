0adb6c6b761bebb6424c8392f33d42755e6fb98a
AbdelhakA <asserrar.abdelhak@gmail.com>
2022-02-28 01:23:21 +0100

    news

--- back/controllers/Sauce.js
+++ back/controllers/Sauce.js
@@ -1,20 +1,37 @@
 const Sauce = require('..models/Sauce');
 
 exports.createSauce = (req, res, next) => { // Methode POST
-    delete req.body._id; 
+  const sauceObject = JSON.parse(req.body.sauce);  
+  delete sauceObject._id; 
     const sauce = new Sauce({
-       ...req.body.sauce
+       ...sauceObject,
+       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}` // composition de l'URL de l'image
      });
      sauce.save()
      .then(() => res.status(201).json({ message: "objet enregistré !"}))
      .catch(error => res.status(400).json({ error }));
     };
 
-exports.deleteSauce = (req, res, next) => { // Methode DELETE
-    Sauce.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: "objet supprimé !" }))
-    .catch(error => res.status(400).json({ error }));
-  };
+    exports.deleteSauce = (req, res, next) => {
+      Sauce.findOne({ _id: req.params.id}).then(
+        (sauce) => {
+          if(!sauce) { // Si l'objet n'existe pas
+            req.status(404).json({
+              error: new Error('Objet non trouvé !')
+            });
+          }
+          if (sauce.userId !== req.auth.userId) {
+            return res.status(401).json ({
+              error: new Error('Requête non autorisée !')
+            });
+          }
+          Sauce.deleteOne({ _id: req.params.id }) // verif si l'_id de la base de données = celui des paaramètres de la request
+      .then(() => res.status(200).json({ message: "objet supprimé !" }))
+      .catch(error => res.status(400).json({ error }));
+        }
+      )
+      
+    };
 
 exports.modifySauce = (req, res, next) => { // Methode PUT
     Sauce.updateOne({ _id: req.params.id }, {...req.body, _id: req.params.id })
--- back/routes/Sauce.js
+++ back/routes/Sauce.js
@@ -3,25 +3,26 @@ const router = express.Router();
 const Sauce = require('../models/Sauce');
 
 const sauceCtrl = require('../controllers/stuff'); // importe le contenu de controllers/stuff.js ici
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
-
-router.post('/api/stuff', stuffCtrl.createSauce); // stuffCtrl.createThing fait référence à la logique métier qui se trouve dans controllers/stuff.js
+router.post('/', auth, multer, sauceCtrl.createSauce); // stuffCtrl.createThing fait référence à la logique métier qui se trouve dans controllers/stuff.js
   
     // ---------------- SUPPRIMER UN OBJET -------------------------------------
   
-router.delete('/:id', stuffCtrl.deleteSauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
   
     // -------- MODIFIER UN OBJET -----------------------------------------------
   
-router.put('/:id', stuffCtrl.modifySauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
   
    //--------------// requet GET pour UN SEUL THING de la database---------------
   
-router.get('/:id', stuffCtrl.getOneSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
   
     //------------- requet GET pour toutes les Things de la database ------------
     
-router.get('/', stuffCtrl.getAllSauces);
+router.get('/', auth, sauceCtrl.getAllSauces);
 
     
 module.exports = router;
